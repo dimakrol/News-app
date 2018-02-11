03ca0b152cd3e960b5694b2bcb83775a371de8a6
dimakrol <dima.krol@mail.ru>
2018-02-11 19:44:22 +0200

    rebuilding comment functionality

--- src/Comment.js
+++ src/Comment.js
@@ -1,44 +1,11 @@
-import React, {Component} from 'react'
-// import CommentList from './CommentList';
+import React from 'react'
 
-export default class Comment extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isOpen: true
-        };
-
-        // this.toggleOpen = this.toggleOpen.bind(this);
-    }
-    render() {
-        const {isOpen} = this.state;
-        return (
-            <div>
-                <br/>
-                {this.getBody()}
-                <br />
-                <button onClick = {this.toggleOpen} >
-                    {isOpen ? 'close' : 'open'}
-                </button>
-            </div>
-        )
-    }
-
-    getBody() {
-        if (!this.state.isOpen) return null;
-        const {comment} = this.props;
-        return (
-            <div>
-                <section>{comment.text}</section>
-            </div>
-        )
-    }
-
-    toggleOpen = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+function Comment({comment}){
+    return (
+        <div>
+            <p>{comment.text} by: <b>{comment.user}</b></p>
+        </div>
+    )
 }
+
+export default Comment;
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react'
 import Comment from './Comment';
 
-export default function CommentList({comments}) {
-    const commentElements = comments ? comments.map((comment) => <li key={comment.id}><Comment comment = {comment} /></li>) : null;
-    return (
-        <ul>
-            <h4>Comments:</h4>
-            {commentElements}
-        </ul>
-    )
+class CommentList extends Component {
+    state = {
+        isOpen: false
+    };
+
+    render() {
+        const text = this.state.isOpen ? 'hide comments' : 'show comments'
+        return (
+            <div>
+                <button onClick={this.toggleOpen}>{text}</button>
+                {this.getBody()}
+            </div>
+        )
+    }
+
+    getBody() {
+        if (!this.state.isOpen) return null;
+
+        const {comments} = this.props;
+        if (!comments || !comments.length) return <p>No comments yet</p>
+
+        return (
+            <ul>
+                {comments.map(comment => <li key={comment.id}><Comment comment={comment}/></li>)}
+            </ul>
+        )
+    }
+
+    toggleOpen = ev => this.setState({
+        isOpen: !this.state.isOpen
+    })
 }
+
+export default CommentList;
