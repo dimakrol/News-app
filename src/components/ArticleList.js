53f4f6a2fa356c45e8c3e136da6d383d6227ae92
dimakrol <14011987tana>
2018-08-07 21:35:57 +0300

    implemented different way for display filtered articles.

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -17,27 +17,27 @@ class ArticleList extends Component {
     };
 
     render() {
-        const moment = extendMoment(Moment);
-        const {from, to} = this.props.selectedDates;
-        const selectedIds = this.props.selectedArticles.map((selected) => selected.value );
+        // const moment = extendMoment(Moment);
+        // const {from, to} = this.props.selectedDates;
+        // const selectedIds = this.props.selectedArticles.map((selected) => selected.value );
+        //
+        // let articlesToDisplay = this.props.articles;
+        // if (from && to) {
+        //   const momentFrom = moment(from);
+        //   const momentTo = moment(to);
+        //   const range = moment().range(momentFrom, momentTo);
+        //   articlesToDisplay = articlesToDisplay.filter((article) => {
+        //     return range.contains(moment(article.date))
+        //   });
+        // }
+        //
+        // if (selectedIds.length) {
+        //   articlesToDisplay = articlesToDisplay.filter((article) => {
+        //     return selectedIds.includes(article.id)
+        //   });
+        // }
 
-        let articlesToDisplay = this.props.articles;
-        if (from && to) {
-          const momentFrom = moment(from);
-          const momentTo = moment(to);
-          const range = moment().range(momentFrom, momentTo);
-          articlesToDisplay = articlesToDisplay.filter((article) => {
-            return range.contains(moment(article.date))
-          });
-        }
-
-        if (selectedIds.length) {
-          articlesToDisplay = articlesToDisplay.filter((article) => {
-            return selectedIds.includes(article.id)
-          });
-        }
-
-        const articleElements = articlesToDisplay.map((article) =>
+        const articleElements = this.props.articles.map((article) =>
           <li key={article.id}>
             <Article
                 article = {article}
@@ -53,9 +53,25 @@ class ArticleList extends Component {
         )
     }
 }
-export default connect(state => ({
-  articles: state.articles,
-  selectedArticles: state.filters.selected,
-  selectedDates: state.filters.selectedDates
-  // defaultOpenId: state.articles[0].id//default open article id
-}))(accordion(ArticleList));
+// export default connect(state => ({
+//   articles: state.articles,
+//   selectedArticles: state.filters.selected,
+//   selectedDates: state.filters.selectedDates
+//   // defaultOpenId: state.articles[0].id//default open article id
+// }))(accordion(ArticleList));
+export default connect(({articles, filters}) => {
+  const {selected, selectedDates: {from, to}} = filters;
+
+  const selectedIds = selected.map((selected) => selected.value );
+
+  const filteredArticles = articles.filter(article => {
+    const published = Date.parse(article.date);
+    return (!selected.length || selectedIds.includes(article.id)) &&
+      (!from || !to || (published > from && published < to))
+  })
+
+  return {
+    articles: filteredArticles
+  }
+
+})(accordion(ArticleList))
